d743bafe61811a0aef5c3b2284ba6124ee9f6ca0
YoichiMasa <laurence.jamail@gmail.com>
2015-10-30 01:07:06 -0400

    Commit from Desktop App

---
+++ Source/ProgrammerProject/ProgrammerProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProgrammerProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMERPROJECT_API AProgrammerProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
